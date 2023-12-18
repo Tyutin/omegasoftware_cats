8ab3417b7cae19fe94656782c4b89db8345edb8e
Tyutin Kirill <kerya.tyutin@yandex.ru>
2023-12-19 02:26:38 +0400

    header mobile

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,55 @@
-import Nav from '../Nav/Nav';
+import useIsMobile from '../../hooks/useIsMobile';
 
 import './Header.scss';
+import { useState } from 'react';
+import classNames from 'classnames';
+import { NavLink } from 'react-router-dom';
 
 export default function Header() {
+  const { isMobile } = useIsMobile();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   return (
     <header className="header">
       <span>Kiki's catpic seller service</span>
-      <Nav />
+      {isMobile && (
+        <button
+          className={classNames(
+            'header__toggle',
+            isMenuOpen && 'header__toggle_active'
+          )}
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+        />
+      )}
+      <nav
+        className={classNames(
+          'header__nav',
+          isMenuOpen && isMobile && 'header__nav_active'
+        )}
+        onClick={() => setIsMenuOpen(false)}
+      >
+        <ul className="header__nav-list">
+          <li className="header__nav-el">
+            <NavLink className="header__nav-link" to={'/'}>
+              Главная
+            </NavLink>
+          </li>
+          <li className="header__nav-el">
+            <NavLink className="header__nav-link" to={'/find'}>
+              Поиск
+            </NavLink>
+          </li>
+          <li className="header__nav-el">
+            <NavLink className="header__nav-link" to={'/favorites'}>
+              Избранное
+            </NavLink>
+          </li>
+          <li className="header__nav-el">
+            <NavLink className="header__nav-link" to={'/cart'}>
+              Корзина
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 }
